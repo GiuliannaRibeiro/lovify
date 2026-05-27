47655aee96fe350f6e84ffe412e48b910fb7e646
Giulianna Ribeiro <giuliannadev@gmail.com>
2026-05-26 23:13:28 -0300

    feat: add playlist and track types

---
+++ src/components/playlist/types.ts
@@ -0,0 +1,12 @@
+export type Track = {
+  id: string;
+  name: string;
+  duration: string;
+};
+
+export type Playlist = {
+  id: string;
+  name: string;
+  cover?: string;
+  tracks: Track[];
+};
